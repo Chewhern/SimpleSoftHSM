25d717f490e5a26f8c9ea5da33191adb35ebd08d
Chrono <chrono_legionnaire@protonmail.com>
2023-03-21 11:58:17 +0800

    Add files via upload

---
+++ Source/0.0.1/Initialization.cs
@@ -0,0 +1,116 @@
+﻿using ASodium;
+using System.Runtime.InteropServices ;
+using BCASodium;
+using Org.BouncyCastle.Math.EC.Rfc8032;
+
+namespace SimpleSoftHSM
+{
+    public class Initialization
+    {
+        private static Byte[] publicKey = new Byte[] { };
+        private static Boolean IsED25519;
+        private static Boolean IsInitialized;
+        private static Byte[] RandomChallenge = new Byte[] { };
+        private static DateTime ChallengeGenerateDT;
+        private static Boolean IsAuthorized;
+
+        public Initialization(Byte[] PublicKey) 
+        {
+            if (PublicKey == null || PublicKey.Length==0) 
+            {
+                throw new ArgumentException("Error: Public key should not be null");
+            }
+            if (PublicKey.Length != SodiumPublicKeyAuth.GetPublicKeyBytesLength() || PublicKey.Length != Ed448.PublicKeySize) 
+            {
+                throw new ArgumentException("Error: Public key must belong to either ED448 or ED25519");
+            }
+            publicKey = PublicKey;
+            if (PublicKey.Length == SodiumPublicKeyAuth.GetPublicKeyBytesLength())
+            {
+                IsED25519 = true;
+            }
+            else 
+            {
+                IsED25519 = false;
+            }
+            IsInitialized = true;
+        }
+
+        public static Byte[] RequestChallenge() 
+        {
+            RandomChallenge = SodiumRNG.GetRandomBytes(128);
+            ChallengeGenerateDT = DateTime.UtcNow.AddHours(8);
+            return RandomChallenge;
+        }
+
+        public static Boolean GetIsAuthorized() 
+        {
+            return IsAuthorized; 
+        }
+
+        public static Byte[] GetPublicKey() 
+        {
+            return publicKey;
+        }
+
+        public static DateTime GetChallengeGenerateDT() 
+        {
+            return ChallengeGenerateDT;
+        }
+
+        public static void Authorize(Byte[] SignedChallenge) 
+        {
+            if (SignedChallenge == null || SignedChallenge.Length == 0) 
+            {
+                throw new ArgumentException("Error: Signed Challenge can't be null or empty");
+            }
+
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] Challenge = new Byte[] { };
+
+            if (Duration.TotalMinutes > 8)
+            {
+                IsAuthorized = false;
+            }
+            else 
+            {
+                try
+                {
+                    if (IsInitialized == true)
+                    {
+                        if (IsED25519 == true)
+                        {
+                            Challenge = SodiumPublicKeyAuth.Verify(SignedChallenge, publicKey);
+                        }
+                        else 
+                        {
+                            Challenge = SecureED448.GetMessageFromSignatureMessage(publicKey, SignedChallenge, new byte[] { });
+                        }
+                        GCHandle ChallengeGCHandle = GCHandle.Alloc(Challenge, GCHandleType.Pinned);
+                        GCHandle ChallengeGCHandle2 = GCHandle.Alloc(RandomChallenge, GCHandleType.Pinned);
+                        try
+                        {
+                            SodiumHelper.Sodium_Memory_Compare(ChallengeGCHandle.AddrOfPinnedObject(), ChallengeGCHandle2.AddrOfPinnedObject(),128);                            
+                            IsAuthorized = true;
+                        }
+                        catch 
+                        {
+                            IsAuthorized = false;
+                        }
+                        ChallengeGCHandle.Free();
+                        ChallengeGCHandle2.Free();
+                    }
+                    else 
+                    {
+                        throw new ArgumentException("Error: Public key has not been initialized");
+                    }
+                }
+                catch 
+                {
+                    IsAuthorized = false;   
+                }
+            }
+        }
+    }
+}
---
+++ Source/0.0.1/SecretKeyOperation.cs
@@ -0,0 +1,318 @@
+﻿using ASodium;
+using System.Runtime.InteropServices;
+
+namespace SimpleSoftHSM
+{
+    public class SecretKeyOperation
+    {
+        private static IntPtr SecretKeyIntPtr;
+        private static Boolean IsInitialized;
+        private static Boolean HasSodiumInitialized;
+        private static int Count;
+
+        public static void LoadSecretKey(Byte[] Key) 
+        {
+            if (Key == null || Key.Length==0) 
+            {
+                throw new ArgumentException("Error: Key can't be null or empty");
+            }
+            if (Key.Length != 32) 
+            {
+                throw new ArgumentException("Error: Key length must be 32 bytes or 256 bits in length");
+            }
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else 
+            {
+                Boolean IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Boolean IsZero = false;
+                    if (HasSodiumInitialized == false)
+                    {
+                        SodiumInit.Init();
+                        HasSodiumInitialized = true;
+                    }
+                    if (IsInitialized == true)
+                    {
+                        SodiumGuardedHeapAllocation.Sodium_MProtect_ReadWrite(SecretKeyIntPtr);
+                    }
+                    else
+                    {
+                        SecretKeyIntPtr = SodiumGuardedHeapAllocation.Sodium_Malloc(ref IsZero, 32);
+                        Count = 0;
+                        while (IsZero == true && Count < 10)
+                        {
+                            SecretKeyIntPtr = SodiumGuardedHeapAllocation.Sodium_Malloc(ref IsZero, 32);
+                            Count += 1;
+                        }
+                    }
+                    Marshal.Copy(Key, 0, SecretKeyIntPtr, 32);
+                    IsInitialized = true;
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                }
+                else 
+                {
+                    throw new ArgumentException("Error: Access Denied");
+                }
+            }            
+        }
+
+        public static Byte[] XChaCha20HMACEncrypt(Byte[] Message) 
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] CipherTextWithMAC = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else 
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Nonce = SodiumGenericHash.ComputeHash(24, Initialization.GetPublicKey());
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    CipherTextWithMAC = EDSStreamCipher.XChaCha20HMACEncrypt(Message, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else 
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return CipherTextWithMAC;
+        }
+
+        public static Byte[] XChaCha20HMACDecrypt(Byte[] CipherTextWithMAC) 
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] Message = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Nonce = SodiumGenericHash.ComputeHash(24, Initialization.GetPublicKey());
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    Message = EDSStreamCipher.XChaCha20HMACDecrypt(CipherTextWithMAC, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return Message;
+        }
+
+        public static Byte[] XSalsa20HMACEncrypt(Byte[] Message) 
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] CipherTextWithMAC = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Nonce = SodiumGenericHash.ComputeHash(24, Initialization.GetPublicKey());
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    CipherTextWithMAC = EDSStreamCipher.XSalsa20HMACEncrypt(Message, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return CipherTextWithMAC;
+        }
+
+        public static Byte[] XSalsa20HMACDecrypt(Byte[] CipherTextWithMAC) 
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] Message = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Nonce = SodiumGenericHash.ComputeHash(24, Initialization.GetPublicKey());
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    Message = EDSStreamCipher.XSalsa20HMACDecrypt(CipherTextWithMAC, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return Message;
+        }
+
+        public static Byte[] XChaCha20HMACEncrypt(Byte[] Message, Byte[] Nonce)
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] CipherTextWithMAC = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    CipherTextWithMAC = EDSStreamCipher.XChaCha20HMACEncrypt(Message, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return CipherTextWithMAC;
+        }
+
+        public static Byte[] XChaCha20HMACDecrypt(Byte[] CipherTextWithMAC, Byte[] Nonce)
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] Message = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    Message = EDSStreamCipher.XChaCha20HMACDecrypt(CipherTextWithMAC, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return Message;
+        }
+
+        public static Byte[] XSalsa20HMACEncrypt(Byte[] Message, Byte[] Nonce)
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] CipherTextWithMAC = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    CipherTextWithMAC = EDSStreamCipher.XSalsa20HMACEncrypt(Message, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return CipherTextWithMAC;
+        }
+
+        public static Byte[] XSalsa20HMACDecrypt(Byte[] CipherTextWithMAC, Byte[] Nonce)
+        {
+            Boolean IsAuthorized = true;
+            DateTime ChallengeGenerateDT = Initialization.GetChallengeGenerateDT();
+            DateTime CurrentDateTime = DateTime.UtcNow.AddHours(8);
+            TimeSpan Duration = CurrentDateTime.Subtract(ChallengeGenerateDT);
+            Byte[] Message = new Byte[] { };
+            if (Duration.TotalMinutes > 8)
+            {
+                throw new ArgumentException("Error: Please request another challenge");
+            }
+            else
+            {
+                IsAuthorized = Initialization.GetIsAuthorized();
+                if (IsAuthorized == true)
+                {
+                    Byte[] Key = new Byte[32];
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_ReadOnly(SecretKeyIntPtr);
+                    Marshal.Copy(SecretKeyIntPtr, Key, 0, 32);
+                    SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SecretKeyIntPtr);
+                    Message = EDSStreamCipher.XSalsa20HMACDecrypt(CipherTextWithMAC, Nonce, Key);
+                    SodiumSecureMemory.SecureClearBytes(Key);
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Please request another challenge or use another public key");
+                }
+            }
+            return Message;
+        }
+    }
+}
